6d8ebea7a94f44b38d57506e440f4fd17457be54
jaydeep0604 <jaydeepkoladiya64@gmail.com>
2023-07-21 11:22:26 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web/ui/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
---
+++ lib/ui/home_screen.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web/widget/bottom_bar.dart';
+import 'package:flutter_web/widget/carousel.dart';
+import 'package:flutter_web/widget/destination_heading.dart';
+import 'package:flutter_web/widget/explore_drawer.dart';
+import 'package:flutter_web/widget/featured_heading.dart';
+import 'package:flutter_web/widget/featured_tiles.dart';
+import 'package:flutter_web/widget/floating_quick_access_bar.dart';
+import 'package:flutter_web/widget/responsive.dart';
+import 'package:flutter_web/widget/top_bar_contents.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<bool> _isHovering = [false, false, false, false];
+  final ScrollController _scrollController = ScrollController();
+  // ignore: unused_field
+  double _scrollPosition = 0;
+  // ignore: unused_field
+  double _opacity = 0;
+
+  _scrollListener() {
+    setState(() {
+      _scrollPosition = _scrollController.position.pixels;
+    });
+  }
+
+  @override
+  void initState() {
+    _scrollController.addListener(_scrollListener);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    _opacity = _scrollPosition < screenSize.height * 0.40
+        ? _scrollPosition / (screenSize.height * 0.40)
+        : 1;
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: ResponsiveWidget.isSmallScreen(context)
+          ? AppBar(
+              // for smaller screen sizes
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              title: Text(
+                'EXPLORE',
+                style: TextStyle(
+                  color: Colors.blueGrey.shade100,
+                  fontSize: 20,
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 3,
+                ),
+              ),
+            )
+          : PreferredSize(
+              // for larger & medium screen sizes
+              preferredSize: Size(screenSize.width, 1000),
+              child: TopBarContents(_opacity),
+            ),
+      drawer: ExploreDrawer(),
+      body: SingleChildScrollView(
+        controller: _scrollController,
+        physics: ClampingScrollPhysics(),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  // image below the top bar
+                  child: SizedBox(
+                    height: screenSize.height * 0.45,
+                    width: screenSize.width,
+                    child: Image.asset(
+                      'assets/images/cover.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Column(
+                  children: [
+                    FloatingQuickAccessBar(screenSize: screenSize),
+                    Container(
+                      child: Column(
+                        children: [
+                          FeaturedHeading(
+                            screenSize: screenSize,
+                          ),
+                          FeaturedTiles(screenSize: screenSize)
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            DestinationHeading(screenSize: screenSize),
+            DestinationCarousel(),
+            SizedBox(height: screenSize.height / 10),
+            BottomBar(),
+          ],
+        ),
+      ),
+    );
+  }
+}
